74f411d579af0b259c9b56275effe1cdab4cb52b
Rajarshee Das <72183674+BupuBupu@users.noreply.github.com>
2023-07-12 13:07:17 +0530

    Create SieveOfEratosthenes.cpp

---
+++ SieveOfEratosthenes.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+n= 10;
+bool prime[n + 1];
+memset(prime, true, sizeof(prime)); 
+for (int p = 2; p * p <= n; p++) {
+    if (prime[p] == true) {
+       for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+    }
+}
